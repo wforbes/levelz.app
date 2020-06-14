b53788aad721e208e57d39ff56eefb51c81e8ee7
Will Forbes <wforbes@users.noreply.github.com>
2020-06-14 16:50:40 -0700

    add favicon and pwa icons
    - added favicon
    -- used https://realfavicongenerator.net/ and https://favicon.io/favicon-generator/
    - you can now add it to your home screen on android, untested on iphone

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,27 @@
 module.exports = {
   "transpileDependencies": [
     "vuetify"
-  ]
+  ],
+  pwa: {
+    name: 'Levelz',
+    themeColor: '#209CEE',
+    msTileColor: '#000000',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: '209CEE',
+
+    // configure the workbox plugin
+    workboxPluginMode: 'InjectManifest',
+    workboxOptions: {
+      // swSrc is required in InjectManifest mode.
+      swSrc: 'src/registerServiceWorker.js',
+      // ...other Workbox options...
+	},
+	iconPaths: {
+		favicon32: 'img/icons/favicon-32x32.png',
+		favicon16: 'img/icons/favicon-16x16.png',
+		appleTouchIcon: 'img/icons/apple-touch-icon.png',
+		maskIcon: 'img/icons/safari-pinned-tab.svg',
+		msTileImage: 'img/icons/mstile-150x150.png'
+	}
+  }
 }
