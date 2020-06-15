8949fa04794758153847f71f889e340f4cd8b876
Will Forbes <wforbes@users.noreply.github.com>
2020-06-14 21:12:44 -0700

    added a link to home page from toolbar icon - also added a hover elevation effect to it

--- src/app/components/AppBar.vue
+++ src/app/components/AppBar.vue
@@ -4,7 +4,15 @@
 			app
 			dark
 		>
-			<v-img src="../../assets/logo-icon.png" max-width="42" />
+			<v-hover
+				v-slot:default="{ hover }"
+			>
+				<v-card :elevation="hover ? 12 : 2">
+					<router-link to="/">
+						<v-img src="../../assets/logo-icon.png" max-width="42" />
+					</router-link>
+				</v-card>
+			</v-hover>
 			<v-spacer></v-spacer>
 			<v-btn icon class="mr-3">
 				<v-icon>mdi-key</v-icon>
