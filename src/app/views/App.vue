ce356420378f6dd8cb5869eda69a914eb43dc2bf
Will Forbes <wforbes@users.noreply.github.com>
2020-06-14 20:56:17 -0700

    Add Footer component to App

--- src/app/views/App.vue
+++ src/app/views/App.vue
@@ -4,15 +4,17 @@
 		<v-main>
 			<router-view></router-view>
 		</v-main>
+		<Footer />
 	</v-app>
 </template>
 
 <script>
 import AppBar from "../components/AppBar.vue";
+import Footer from "../components/Footer.vue";
 export default {
 	name: 'App',
 	components: {
-		AppBar,
+		AppBar, Footer
 	},
 	data(){
 		return {
