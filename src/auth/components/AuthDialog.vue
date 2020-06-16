34ee541eb0f4fe608654095d340f69968c6c6ea7
Will Forbes <wforbes@users.noreply.github.com>
2020-06-16 13:37:29 -0700

    Email and Data Statement - adjusted the width of the dialog to match the AuthDialog - expanded the text a bit, now mentioning  analytics, login services, and offline features.

--- src/auth/components/AuthDialog.vue
+++ src/auth/components/AuthDialog.vue
@@ -71,7 +71,7 @@
 				</v-tabs>
 			</v-card>
 		</v-dialog>
-		<v-dialog v-model="moreInfoOpen">
+		<v-dialog v-model="moreInfoOpen" max-width="800">
 			<v-toolbar dark dense flat>
 				<v-card-title>Lvlz Email and Data Statement</v-card-title>
 				<v-spacer></v-spacer>
@@ -80,29 +80,51 @@
 				</v-btn>
 			</v-toolbar>
 			<v-card class="pa-3 pr-6 pl-6">
-				<!--<h1>Levelz.app Email and Data Statement</h1>-->
 				<h3>We don't spam. Period.</h3>
 				<ul>
-					<li>We won't send messages you haven't agreed to.</li>
-					<li>We won't ever send you messages you can't turn off or unsubscribe from.</li>
-					<li>We won't send you messages from sources other than Levelz.app or Lvlz.app.</li>
+					<li>We won't send you emails you can't unsubscribe from or turn off.</li>
+					<li>We won't send you emails from sources other than addresses at Levelz.app or Lvlz.app.</li>
+					<li>We won't send you emails that you haven't specifically agreed to wanting to get.</li>
 				</ul>
 				<h3>We don't share your data with Third-Parties. Period.</h3>
 				<p>
 					We don't agree with sharing your data with any other entity outside of
 					Levelz.app or Lvlz.app, in principle and practice. Not only do we value your privacy 
 					and security, but we also find it very important to keep you from experiencing 
-					the intrusive ads and solicitation that comes from third-party data companies
-					when they get ahold of your data and spread it around to all their friends.
+					the intrusive ads and solicitation that come from third-party data companies
+					when they get ahold of your data and spread it around to all their friends. If we
+					do ever share your data, it will be within your full control to limit it, or because
+					specifically requested it! 
+				</p>
+				<h4>Analytics and Usage Data</h4>
+				<p>
+					Although we do use your browsing data to make Lvlz.app better when we can, and a part
+					of that may involve using the Google Analytics service, we'll always do our best to keep
+					whatever analytics data we have about individuals private.
+				</p>
+				<h4>Third-Party Login Services</h4>
+				<p>
+					Although we may offer log in services from companies like Google or Facebook to provide
+					you the convience of logging in to Lvlz.app using your accounts on those other platforms, 
+					we'll always do our best to limit our data sharing with those platforms so that we can conserve
+					your privacy.
+				</p>
+				<h4>Goals For Offline Features</h4>
+				<p>
+					In our general development plan for Lvlz.app we've outlined various ways that we can offer
+					offline, private, non-connected, self-contained versions of our features so that you may use Lvlz.app
+					without even being connected to the internet, and thus not even sharing your data with us, 
+					unless you want to. We will continue to pursue paradigms like this because we support your
+					desire to keep your data private.
 				</p>
 				<h4>We'll keep getting better at this.</h4>
 				<p>
 					Lvlz is a very young app and we're interested in finding new ways to improve this
 					statement.<br />
-					Email us with your thoughts at <a href="mailto:will@levelz.app">will@levelz.app</a>.
+					Email me with your thoughts at <a href="mailto:will@levelz.app">will@levelz.app</a>.
 				</p>
+				<p>- Will Forbes, Creator of Lvlz</p>
 			</v-card>
-			
 		</v-dialog>
 	</div>
 </template>
