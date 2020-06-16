81a1512eed55375253d14f1f66e4bbeee651fc37
Will Forbes <wforbes@users.noreply.github.com>
2020-06-16 12:20:53 -0700

    added basic eslintrc config file - it seems like this helps vscode to give in line lint errors, not sure!

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+	root: true,
+	env: {
+		node: true
+	},
+	extends: ["plugin:vue/essential", "@vue/prettier"],
+	rules: {
+		"no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+		"no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+		"indent": ["error", "tab"],
+		"prettier/prettier": [
+			"warn", {
+				useTabs: true
+			}
+		]
+	},
+	parserOptions: {
+		parser: "babel-eslint"
+		}
+	};
