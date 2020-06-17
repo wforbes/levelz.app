f9764c392faf397a58e94951102113246738f7f8
Will Forbes <wforbes@users.noreply.github.com>
2020-06-16 21:46:05 -0700

    Changed color theme slightly - lightened up the primary color to match icon logo

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -3,4 +3,18 @@ import Vuetify from "vuetify/lib";
 
 Vue.use(Vuetify);
 
-export default new Vuetify({});
+export default new Vuetify({
+	theme: {
+		themes: {
+			light: {
+				primary: "#209CEE",
+				secondary: "#424242",
+				accent: "#82B1FF",
+				error: "#FF5252",
+				info: "#2196F3",
+				success: "#4CAF50",
+				warning: "#FFC107"
+			}
+		}
+	}
+});
