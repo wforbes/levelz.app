4c9cc0673956e39c44bd08dcb53849941d3a5dd5
Will Forbes <wforbes@users.noreply.github.com>
2020-06-17 14:19:47 -0700

    PHP - update comments on index.php

--- api/index.php
+++ api/index.php
@@ -1,5 +1,5 @@
 <?php
-// skip OPTIONS method
+// part of initial CORS check on local dev environment
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
 	header('Access-Control-Allow-Origin: http://localhost:8080');
 	header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
@@ -9,10 +9,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
 	header('Content-Type: text/plain');
 	die();
 }
-// allow every url
+// allow local dev environment requests
 header('Access-Control-Allow-Origin:  http://localhost:8080');
 header('Access-Control-Allow-Methods:  POST, GET, OPTIONS, PUT, DELETE');
-// allow content type !!IMPORTANT
 header('Content-Type: application/json');
 
 require "src/Core/App.php";
