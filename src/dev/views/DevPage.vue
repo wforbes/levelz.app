0ab5815ec089125eefd1827302e01215bde3d376
Will Forbes <wforbes@users.noreply.github.com>
2020-06-16 20:24:12 -0700

    eslint: handle no-console - to allow 'warn' and 'error'

--- src/dev/views/DevPage.vue
+++ src/dev/views/DevPage.vue
@@ -48,7 +48,7 @@ export default {
 					this.testGetResponse = response.data;
 				})
 				.catch(error => {
-					console.log(error);
+					console.error(error);
 				});
 		},
 		clearTestGetResponse() {
