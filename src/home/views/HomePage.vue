b3e856934522f36aadc0f1338d12e088e6df5d01
Will Forbes <wforbes@users.noreply.github.com>
2020-06-21 16:01:28 -0700

    Update HomePage.vue

--- src/home/views/HomePage.vue
+++ src/home/views/HomePage.vue
@@ -11,12 +11,39 @@
 					</span>
 				</v-col>
 			</v-row>
+		</div>
+		<div class="home">
+			<v-container fluid>
+				<v-row>
+					<v-col>
+						<h1>What is Levelz?</h1>
+						<p>
+							Levelz is a productivity web app that gamifies the concept of a
+							todo list or task tracking app. We try to help you reinforce good
+							habits and level up your life.
+						</p>
+						<h1>Project Status</h1>
+						<p>
+							This app is currently in its very early stages of development.<br />
+							We are an open-source project. More info:
+							<a href="https://github.com/wforbes/levelz.app" target="_blank"
+								>github.com/wforbes/levelz.app</a
+							><br /><br />
+							Please contact
+							<a href="mailto:will@levelz.app">will@levelz.app</a>
+							with any questions!
+						</p>
+					</v-col>
+				</v-row>
+			</v-container>
+		</div>
+		<div class="project-updates">
 			<v-row>
 				<v-col>
 					<div
 						style="display:inline-block; border: 0.1em solid black; border-bottom: 0.1em; padding: 0.2em; border-top-left-radius: 0.2em; border-top-right-radius: 0.2em; "
 					>
-						<h4>updates:</h4>
+						<h4>Project Updates:</h4>
 					</div>
 					<div
 						style="border: 0.1em solid black; border-bottom: 0.25em solid black; border-radius:0.2em; padding: 1em;"
@@ -37,27 +64,6 @@
 					</div>
 				</v-col>
 			</v-row>
-		</div>
-		<div class="home">
-			<v-container fluid>
-				<v-row>
-					<v-col>
-						<h1>What is Levelz?</h1>
-						<p>
-							Levelz is a productivity web app that gamifies the concept of a
-							todo list or task tracking app. We try to help you reinforce good
-							habits and level up your life.
-						</p>
-						<h1>Project Status</h1>
-						<p>
-							This app is currently in its very early stages of development.
-							Please contact
-							<a href="mailto:will@levelz.app">will@levelz.app</a>
-							with any questions!
-						</p>
-					</v-col>
-				</v-row>
-			</v-container>
 		</div>
 		<br /><br />
 	</div>
