06ae3313b2f371c9d2d03aa9aac94f927711e81c
Will Forbes <wforbes@users.noreply.github.com>
2020-07-04 13:51:15 -0700

    Project Update and some Home changes - Added a project update from the recent work - Reworded the opening intro v-card - Added a link to the new About page

--- src/home/views/HomePage.vue
+++ src/home/views/HomePage.vue
@@ -19,9 +19,17 @@
 						<v-card class="pa-3 mb-4">
 							<h1>What is Levelz?</h1>
 							<p>
-								Levelz is a productivity web app that gamifies the concept of a
-								todo list or task tracking app. <br />
-								We try to help you reinforce good habits and level up your life.
+								Levelz is a productivity web app that looks to help you
+								reinforce your good habits, get organized, stay productive, and
+								level up in life!
+							</p>
+							<p>
+								For more info, you can read all about the app on
+								<router-link to="about">lvlz.app/about</router-link>!
+							</p>
+							<p>
+								For the most up to date info on what's been added to the app,
+								click on "Project Updates" below!
 							</p>
 						</v-card>
 						<v-expansion-panels v-model="openPanels" focusable accordion>
@@ -37,21 +45,23 @@
 										<h3>Project Status</h3>
 										<p>
 											This app is currently in its very early stages of
-											development. During 2020 I'll just be adding little
-											features to test out ideas, then reassess the app moving
-											forward next year.<br />
+											development. For the rest of 2020, I'll just be adding
+											little features to test out ideas. Then next year we'll
+											reassess the app moving forward!<br />
 											This is an open-source project. (
 											<a
 												href="http://www.gnu.org/licenses/gpl-3.0.html"
 												target="_blank"
-												>GNU Public License v3.0</a
+											>
+												GNU Public License v3.0</a
 											>
 											)<br />
 											More info:
 											<a
 												href="https://github.com/wforbes/levelz.app"
 												target="_blank"
-												>github.com/wforbes/levelz.app</a
+											>
+												github.com/wforbes/levelz.app </a
 											><br /><br />
 											Please contact
 											<a href="mailto:will@levelz.app">will@levelz.app</a>
@@ -69,6 +79,92 @@
 								</v-expansion-panel-header>
 								<v-expansion-panel-content>
 									<ul style="list-style: none; padding:1em 0; margin:0;">
+										<li>
+											<v-card class="pa-2 mb-3">
+												<h4>
+													(7/3/2020) FeatureMenu, Activites, NavMenu,
+													UserDialog, and more
+												</h4>
+												<strong>Features Menu</strong>
+												<p>
+													There's a 'Features Menu' of icon buttons on the User
+													Dashboard now that link to the various pages. Most of
+													the pages are very blank, but the Activities page was
+													started this week.
+												</p>
+												<strong>Experimenting on Activities Page</strong>
+												<p>
+													Clicking the Activities icon on the Dashboard or
+													visiting
+													<router-link to="activities">
+														lvlz.app/activities
+													</router-link>
+													will bring you to the Activities Page. Here
+													experiments were started with a workflow to create an
+													Activity and add Actions to it. So far I don't like
+													it, so it'll be changing quite a bit this month, but
+													it's a start! :) Next week I'll be switching gears to
+													start with simple Activities that are quick to set up
+													and complete, then building Actions into it. I made
+													some user story 'issues' on the
+													<a
+														href="https://github.com/wforbes/levelz.app"
+														target="_blank"
+														>lvlz github</a
+													>
+													and I'll be working on those next.
+												</p>
+												<strong>Navigation Menu</strong>
+												<p>
+													A Navigation Menu was added, you can get to it by
+													clicking the menu icon in the toolbar at the very top
+													left of the app.
+												</p>
+												<strong>About Page</strong>
+												<p>
+													Clicking the About menu item on the NavMenu or
+													visiting
+													<router-link to="about">
+														lvlz.app/about
+													</router-link>
+													will bring you to the About Page. I tried to add as
+													much information here as I could about my plans for
+													the app. I'll be updating this as the app progresses
+													and trimming it down as features become available.
+												</p>
+												<strong>Contact Page</strong>
+												<p>
+													Clicking the Contact menu item on the NavMenu or
+													visiting
+													<router-link to="contact">
+														lvlz.app/contact
+													</router-link>
+													will bring you to the Contact Page. It's really basic,
+													but I'd like to add a chat/support feature here
+													eventually.
+												</p>
+												<strong>User Menu Dialog</strong>
+												<p>
+													After you sign up or you log in, the 'key' icon in the
+													upper right of the screen turns into a 'user' icon.
+													Clicking that 'user' icon will open the User Dialog
+													menu. This provides quick links to get back to the
+													User Dashboard, go to the Account Management page
+													(empty for now), or Logout of the app.
+												</p>
+												<strong>Studying Habitica.com</strong>
+												<p>
+													<a href="https://habitica.com" target="_blank"
+														>Habitica.com</a
+													>
+													is an open source app for Web, Android and iOS that
+													provides a very similar experience to what I hope to
+													achieve on Levelz.app one day. I started studying
+													Habitica to get inspired with ideas and I plan on
+													looking in to contributing to the codebase eventually.
+												</p>
+											</v-card>
+										</li>
 										<li>
 											<v-card class="pa-2 mb-3">
 												<strong>(6/24/2020) User Dashboard Layout</strong><br />
