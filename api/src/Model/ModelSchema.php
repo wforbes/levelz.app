b920f8a5963d2748cde2636c435e77217209d024
Will Forbes <wforbes@users.noreply.github.com>
2020-07-16 23:48:28 -0700

    Add model schema php class
    Working out the full schema for the activity/actions features to come. The Model Schema will help keep the table meta info together and provide a place for table meta data for tables without a php model class (like join tables and feature specific stuff that have its data access functions in a main model)

---
+++ api/src/Model/ModelSchema.php
@@ -0,0 +1,15 @@
+<?php
+
+class ModelSchema {
+	private $tables = [
+		"Users",
+		"UserProfiles",
+		"Activities",
+		"Actions",
+		"Actions_ActionTypes",
+		"ActionTypes",
+		"ActionGradeSets",
+		"ActionGrades",
+		"RelatedActions"
+	];
+}
