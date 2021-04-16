70654ca1a57591320d8a16e2d06a113f8b9e43c6
Will Forbes <wforbes87@gmail.com>
2021-04-16 12:16:08 -0700

    Add localhost SSL cert information
    - Add instructions to set up https for dev environment
    - Add vue.config for SSL certs
    - Add gitignore for my cert files

--- vue.config.js
+++ vue.config.js
@@ -1,27 +1,33 @@
-module.exports = {
-  "transpileDependencies": [
-    "vuetify"
-  ],
-  pwa: {
-    name: 'Levelz',
-    themeColor: '#209CEE',
-    msTileColor: '#000000',
-    appleMobileWebAppCapable: 'yes',
-    appleMobileWebAppStatusBarStyle: '209CEE',
+const fs = require("fs");
 
-    // configure the workbox plugin
-    workboxPluginMode: 'InjectManifest',
-    workboxOptions: {
-      // swSrc is required in InjectManifest mode.
-      swSrc: 'src/registerServiceWorker.js',
-      // ...other Workbox options...
+module.exports = {
+	transpileDependencies: ["vuetify"],
+	pwa: {
+		name: "Levelz",
+		themeColor: "#209CEE",
+		msTileColor: "#000000",
+		appleMobileWebAppCapable: "yes",
+		appleMobileWebAppStatusBarStyle: "209CEE",
+
+		// configure the workbox plugin
+		workboxPluginMode: "InjectManifest",
+		workboxOptions: {
+			// swSrc is required in InjectManifest mode.
+			swSrc: "src/registerServiceWorker.js"
+			// ...other Workbox options...
+		},
+		iconPaths: {
+			favicon32: "img/icons/favicon-32x32.png",
+			favicon16: "img/icons/favicon-16x16.png",
+			appleTouchIcon: "img/icons/apple-touch-icon.png",
+			maskIcon: "img/icons/safari-pinned-tab.svg",
+			msTileImage: "img/icons/mstile-150x150.png"
+		}
 	},
-	iconPaths: {
-		favicon32: 'img/icons/favicon-32x32.png',
-		favicon16: 'img/icons/favicon-16x16.png',
-		appleTouchIcon: 'img/icons/apple-touch-icon.png',
-		maskIcon: 'img/icons/safari-pinned-tab.svg',
-		msTileImage: 'img/icons/mstile-150x150.png'
+	devServer: {
+		https: {
+			key: fs.readFileSync("./certs/localhost-key.pem"),
+			cert: fs.readFileSync("./certs/localhost.pem")
+		}
 	}
-  }
-}
+};
