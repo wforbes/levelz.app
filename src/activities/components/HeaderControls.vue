b63b1a8f714a90606cd53cf1979365fd945367b0
Will Forbes <wforbes87@gmail.com>
2021-05-08 13:05:44 -0700

    Update HeaderControls.vue
    improve the layout of HeaderControls on Activity page, fits better on mobile and markdown structure is more accurate

--- src/activities/components/HeaderControls.vue
+++ src/activities/components/HeaderControls.vue
@@ -1,6 +1,6 @@
 <template>
 	<v-row>
-		<v-col cols="11" class="pa-0 pl-4 pt-4" style="min-height:3em">
+		<v-col cols="11" class="pa-0 pl-4 pt-2" style="min-height:3em">
 			<v-row>
 				<transition name="fade" mode="out-in">
 					<v-col class="pl-3 pa-0" v-if="stepStates.length === 0">
@@ -8,38 +8,35 @@
 							Activities
 						</h1>
 					</v-col>
-					<v-col v-else cols="11" class="pl-3 pa-0" style="margin-top:-0.6em;">
+					<v-col v-else class="pt-2">
 						<v-row>
-							<v-col class="ml-0 pa-0" md="1" cols="2"></v-col>
-							<v-col class="pl-1 pa-0 mb-n3">
-								<h4>Activities</h4>
+							<v-col v-if="currentStateHasBackBtn" sm="1" cols="2">
+								<v-btn icon color="black" large @click="backStep">
+									<v-icon>mdi-arrow-left</v-icon>
+								</v-btn>
 							</v-col>
-						</v-row>
-						<v-row>
-							<v-col class="pl-3 pa-0">
+							<v-col
+								:class="{
+									'ml-4 mt-5': $vuetify.breakpoint.mdAndUp,
+									'ml-4 mt-1': $vuetify.breakpoint.smAndDown
+								}"
+							>
 								<v-row>
-									<v-col class="pa-0" md="1" cols="2">
-										<v-btn
-											icon
-											color="black"
-											large
-											v-if="currentStateHasBackBtn"
-											@click="backStep"
-										>
-											<v-icon>mdi-arrow-left</v-icon>
-										</v-btn>
-									</v-col>
-									<v-col class="pa-0 pt-2">
-										<h2 style="margin-top:0.3em">
-											<span v-for="(state, i) of stepStates" :key="i">
-												<span style="text-overflow:ellipsis">
-													{{ state.name }}
-												</span>
-												<span v-if="stepStates.length - 1 !== i">
-													<v-icon>mdi-chevron-right</v-icon>
-												</span>
+									<v-col class="pa-0">
+										<span v-if="stepStates.length >= 1">
+											<strong>Activity</strong>
+											<span>
+												<v-icon>mdi-chevron-right</v-icon>
 											</span>
-										</h2>
+										</span>
+										<span v-for="(state, i) of stepStates" :key="i">
+											<span style="display:inline; text-overflow:ellipsis">
+												{{ state.name }}
+											</span>
+											<span v-if="stepStates.length - 1 !== i">
+												<v-icon>mdi-chevron-right</v-icon>
+											</span>
+										</span>
 									</v-col>
 								</v-row>
 							</v-col>
