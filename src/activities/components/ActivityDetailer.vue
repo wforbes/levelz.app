ccb7760e680f60feccd26bedc59a6099d3ee6b47
Will Forbes <wforbes87@gmail.com>
2021-05-12 00:43:09 -0700

    ActivityDetailer UI fine tune
    - Modified the action list max height so that the footer for the page isn't hidden and list stays within bounds of the screen on mobile and desktop
    - removed the search field for now because it seems useless
    - repositioned the config/create action buttons to sit better on the card

--- src/activities/components/ActivityDetailer.vue
+++ src/activities/components/ActivityDetailer.vue
@@ -6,12 +6,13 @@
 					<v-col class="pt-0 pb-0">
 						<v-container class="pa-0">
 							<v-row>
-								<v-col cols="1" class="pa-0 pt-2" align="right">
+								<v-col cols="2" class="pa-0 pt-2 pl-5" align="left">
 									<v-btn fab small @click="openEditActivityForm">
 										<v-icon dark>mdi-cog</v-icon>
 									</v-btn>
 								</v-col>
-								<v-col cols="10" class="pt-0 pb-0 mb-0">
+								<v-col cols="8" class="pt-0 pb-0 mb-0">
+									<!--
 									<v-text-field
 										v-model="listSearchTerm"
 										outlined
@@ -19,8 +20,9 @@
 										placeholder="Search"
 										prepend-inner-icon="mdi-text-search"
 									></v-text-field>
+									-->
 								</v-col>
-								<v-col cols="1" class="pa-0 pt-2" align="left">
+								<v-col cols="2" class="pa-0 pt-2 pr-5" align="right">
 									<v-btn
 										fab
 										small
@@ -86,7 +88,7 @@
 										v-if="actionArray.length > 0 && !listIsLoading"
 										class="pa-1 overflow-y-auto"
 										style="border-radius:0.3em"
-										max-height="52vh"
+										max-height="63vh"
 									>
 										<v-list-item
 											v-for="action in actionArray"
@@ -180,6 +182,7 @@
 											</v-col>
 										</v-row>
 									</v-card>
+									<!--
 									<v-card
 										v-if="
 											listSearchTerm !== '' &&
@@ -203,6 +206,7 @@
 											</v-row>
 										</v-container>
 									</v-card>
+									-->
 								</v-col>
 							</v-row>
 						</v-container>
